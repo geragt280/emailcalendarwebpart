4c2dcc0b9cd1d7a4a4ce44ac95bcd86a07b0908c
fizzahkhan123 <guysr92@gmail.com>
2023-05-26 02:00:34 +0500

    project initialized

---
+++ src/webparts/medMutualCalendarWebpart/components/IMedMutualCalendarWebpartProps.ts
@@ -0,0 +1,7 @@
+export interface IMedMutualCalendarWebpartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/medMutualCalendarWebpart/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IMedMutualCalendarWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'MedMutualCalendarWebpartWebPartStrings' {
+  const strings: IMedMutualCalendarWebpartWebPartStrings;
+  export = strings;
+}
